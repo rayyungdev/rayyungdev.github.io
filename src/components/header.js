b677138f9b63cb877ec7302eb26d1337f6cbf01c
Raymond <thec0okierebel12@gmail.com>
2022-08-22 22:01:30 -0400

    made some minor changes

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = ({location}) => {
   return (
         <div className="container py-2 bg-primary mb-2">
             <div className="row justify-content-center">
-                    <h1 className="mb-3 mb-md-0 text-white text-uppercase font-weight-bold">{location}</h1>
+                    <h1 className="mb-md-0 text-white text-uppercase font-weight-bold text-wrap">{location}</h1>
             </div>
         </div>
   )
